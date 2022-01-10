b7f28d28ddd85d44538881e177177be80ea44cc4
TMBell05 <tyler.m.bell05@gmail.com>
2022-01-10 10:10:19 -0600

    Some tweaks to the previous commit to better follow the newest ARM standards

--- profiles/Wind_Profile.py
+++ profiles/Wind_Profile.py
@@ -278,14 +278,17 @@ class Wind_Profile():
         # Do base_time and time_offset like ARM
         bt = abs((self.gridded_times[0] - dt.datetime(1970, 1, 1)).total_seconds())
         bt_var = main_file.createVariable('base_time', 'i8')
+        bt_var.setncattr('long_name', 'Base time in Epoch')
+        bt_var.setncattr('ancillary_variables', 'time_offset')
         bt_var.setncattr('units', 'seconds since 1970-01-01 00:00:00 UTC')
         bt_var[:] = bt
 
         to = netCDF4.date2num(self.gridded_times,
-                              units='seconds since 1970-01-01 00:00:00 UTC') - bt
-        to_var = main_file.createVariable('time_offset', 'i8', dimensions=('time',))
-        to_var.setncattr('long_name', 'Time offset')
-        to_var.setncattr('units', 'seconds since base_time')
+                              units=f'seconds since {bt:%Y-%m-%d %H:%M:%S UTC}')
+        to_var = main_file.createVariable('time_offset', 'f4', dimensions=('time',))
+        to_var.setncattr('long_name', 'Time offset from base_time')
+        to_var.setncattr('units', f'seconds since {bt:%Y-%m-%d %H:%M:%S UTC}')
+        to_var.setncattr('ancillary_variables', 'base_time')
         to_var[:] = to
 
         main_file.close()
